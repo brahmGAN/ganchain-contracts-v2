1c79807e8091c0fcdd4ba7e33288e084009fab04
Deepu <deepuharry02@gmail.com>
2025-04-07 11:38:07 +0530

    ongoing

--- contracts/node.sol
+++ contracts/node.sol
@@ -8,16 +8,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GanNode is ERC721URIStorage,Ownable
 {
-    uint120 tokenID;
+    uint120 _tokenID;
+    mapping(address => uint120) _totalNodesHeld; 
 
     constructor(address owner) ERC721("Gan-Node","GN") Ownable(owner){}
 
-    function mint(address to,string memory _uri) public onlyOwner
+    function mint(address to,string memory uri) public onlyOwner
     {
-        ++tokenID;
+        ++_tokenID;
 
-        _safeMint(to, tokenID);
-        _setTokenURI(tokenID,_uri);
+        _safeMint(to, _tokenID);
+        _setTokenURI(_tokenID,uri);
+
+        ++_totalNodesHeld[to];
         //emit
     }
 }
