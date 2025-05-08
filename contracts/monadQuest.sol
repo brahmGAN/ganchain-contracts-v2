69f4bed8c96ca05d68221fb32025fd67a4913db7
Deepu <deepuharry02@gmail.com>
2025-05-08 10:08:56 +0530

    Refactor: Made the monad contract ownable due to verification requirement

--- contracts/monadQuest.sol
+++ contracts/monadQuest.sol
@@ -1,19 +1,17 @@
-// SPDX-Liscense-Identifier: MIT
-pragma solidity ^0.8.28; 
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26; 
 
 import "./interfaces/IErrors.sol"; 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Monad is  ERC721URIStorage,Ownable,ERC721Burnable,IErrors
+contract Monad is  ERC721URIStorage,ERC721Burnable,IErrors,Ownable
 {
 
     uint120 public _tokenID;
 
     address public _fundsHandler;
-
-    mapping(address => uint120) public _totalNftHeld;  
 
     constructor(address owner, address fundsHandler) ERC721("GPUNET-Monad-Quest","MQ") Ownable(owner)
     {
@@ -27,7 +25,6 @@ contract Monad is  ERC721URIStorage,Ownable,ERC721Burnable,IErrors
         ++_tokenID;
         _safeMint(msg.sender, _tokenID);
         _setTokenURI(_tokenID,uri);
-        ++_totalNftHeld[msg.sender];
         (bool success,) = payable(_fundsHandler).call{value:msg.value}("");
         if(!success) revert TransferFailed(); 
     }
