2d531daff05bfdba0fdf47860a97d9ff3f60cce0
Deepu <deepuharry02@gmail.com>
2025-05-10 00:05:55 +0530

    Prettier

--- test/node.t.js
+++ test/node.t.js
@@ -3,35 +3,37 @@ const { expect } = require("chai");
 
 describe("Subnet", () => {
   let owner;
-  let seller; 
-  let buyer; 
-  const uri = "https://lavender-puny-mosquito-504.mypinata.cloud/ipfs/QmR3udFV7BDMVt1pUDpsg4Y7jdLFCdpC5VwV7BkT9gMKYJ";
+  let seller;
+  let buyer;
+  const uri =
+    "https://lavender-puny-mosquito-504.mypinata.cloud/ipfs/QmR3udFV7BDMVt1pUDpsg4Y7jdLFCdpC5VwV7BkT9gMKYJ";
   before(async () => {
-    [
-      owner,
-      seller,
-      buyer
-    ] = await ethers.getSigners();
+    [owner, seller, buyer] = await ethers.getSigners();
     nodeFactory = await ethers.getContractFactory("GanNode");
-    nodeProxy = await upgrades.deployProxy(
-      nodeFactory,
-      [],
-      { 
-        initializer: "initialize",
-        from: owner.address
-      },
-    );
+    nodeProxy = await upgrades.deployProxy(nodeFactory, [], {
+      initializer: "initialize",
+      from: owner.address,
+    });
   });
 
   describe("Marketplace", () => {
     it("Should mint node and seller should put a sell order and buyer completes the order", async () => {
-      await nodeProxy.connect(owner).mintNode(seller,uri);
+      await nodeProxy.connect(owner).mintNode(seller, uri);
       await expect(await nodeProxy.ownerOf(1)).to.be.equals(seller);
-      await nodeProxy.connect(owner).setTierPrice([ethers.parseEther("1"),ethers.parseEther("2"),ethers.parseEther("3"),ethers.parseEther("4")]);
-      await nodeProxy.connect(owner).setLockStatus(0,true);
-      await nodeProxy.connect(owner).setLockStatus(1,true);
-      await nodeProxy.connect(seller).sellNodes(1,[1],3);
-      await nodeProxy.connect(buyer).buyNodes(1,0,[1],{value: ethers.parseEther("4")});
+      await nodeProxy
+        .connect(owner)
+        .setTierPrice([
+          ethers.parseEther("1"),
+          ethers.parseEther("2"),
+          ethers.parseEther("3"),
+          ethers.parseEther("4"),
+        ]);
+      await nodeProxy.connect(owner).setLockStatus(0, true);
+      await nodeProxy.connect(owner).setLockStatus(1, true);
+      await nodeProxy.connect(seller).sellNodes(1, [1], 3);
+      await nodeProxy
+        .connect(buyer)
+        .buyNodes(1, 0, [1], { value: ethers.parseEther("4") });
       await expect(await nodeProxy.ownerOf(1)).to.be.equals(buyer);
     });
   });
