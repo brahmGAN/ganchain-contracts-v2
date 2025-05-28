fe0519c91faaa8e07b186416ac307ee4b0b0fd7e
Deepu <deepuharry02@gmail.com>
2025-05-28 13:15:04 +0530

    ongoign

--- contracts/monadQuest.sol
+++ contracts/monadQuest.sol
@@ -6,6 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface IERC721 
+{
+    function balanceOf(address owner) external view returns (uint256 balance);
+}
+
+
 contract Monad is  ERC721URIStorage,ERC721Burnable,IErrors,Ownable
 {
 
@@ -15,6 +21,10 @@ contract Monad is  ERC721URIStorage,ERC721Burnable,IErrors,Ownable
 
     uint120 public _mintPrice; 
 
+    uint120 public _paidUserMintPrice;
+
+    IERC721 constant _oldContract = IERC721(0x0B41968c55145CCa1ad8f82cCA996Bf0BCbB58B8); 
+
     event MonadNftMinted
     (
         address user,
@@ -22,18 +32,30 @@ contract Monad is  ERC721URIStorage,ERC721Burnable,IErrors,Ownable
         uint120 tokenID 
     );
 
-    constructor(address owner, address fundsHandler, uint120 mintPrice) ERC721("GPUNET-Monad-Quest","MQ") Ownable(owner)
+    constructor(address owner, address fundsHandler, uint120 mintPrice, uint120 paidUserMintPrice) ERC721("GPUNET-Monad-Quest","MQ") Ownable(owner)
     {
         _fundsHandler = fundsHandler;
         _mintPrice = mintPrice; 
+        _paidUserMintPrice = paidUserMintPrice;
     }
 
-    /// @dev This is for testing 
-    //https://emerald-abundant-wildfowl-967.mypinata.cloud/ipfs/bafkreig64vpxozqu436mc2of7fecasdxllif64l4omxl2usp6h6rokzfpu
-    // 6 ether 6000000000000000000
+    /// @dev The uri to be passed while minting 
+    // https://emerald-abundant-wildfowl-967.mypinata.cloud/ipfs/bafkreig64vpxozqu436mc2of7fecasdxllif64l4omxl2usp6h6rokzfpu
     function mintMonadNft(string memory uri) public payable
     {
-        if(msg.value != uint256(_mintPrice)) revert incorrectAmount(); 
+        uint120 mintPrice;
+        if(_oldContract.balanceOf(msg.sender) > 0 )
+        {
+            // 5.9 ETH
+            mintPrice = _paidUserMintPrice; 
+        }
+        else
+        {
+            // 6 eth
+            mintPrice = _mintPrice; 
+        } 
+
+        if(msg.value != uint256(mintPrice)) revert incorrectAmount(); 
 
         ++_tokenID;
         _safeMint(msg.sender, _tokenID);
@@ -43,7 +65,12 @@ contract Monad is  ERC721URIStorage,ERC721Burnable,IErrors,Ownable
         emit MonadNftMinted(msg.sender, msg.value,_tokenID);
     }
 
-    function setAmount(uint120 mintPrice) public onlyOwner 
+    function setMintPrice(uint120 mintPrice) public onlyOwner 
+    {
+        _mintPrice = mintPrice; 
+    }
+
+    function setMintPriceForPaidUsers(uint120 mintPrice) public onlyOwner 
     {
         _mintPrice = mintPrice; 
     }
