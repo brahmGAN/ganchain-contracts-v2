3b1ef5e1ed44bfaf17ce608c547121073d9c4772
Deepu <deepuharry02@gmail.com>
2025-07-20 14:22:40 +0530

    USDT vault tested

--- test/usdtVault.t.js
+++ test/usdtVault.t.js
@@ -23,13 +23,22 @@ describe("USDTVault", () => {
     it("Should switch on every user functions", async () => {
         console.log("owner balance before: "+await mockUsdt.balanceOf(owner.address));
         console.log("Contract balance before: "+await mockUsdt.balanceOf(usdtVaultProxy.target));
-        await mockUsdt.connect(owner).approve(usdtVaultProxy.target,69 * 10**6);
         await usdtVaultProxy.connect(owner).setLockStatus(true,0);
         await usdtVaultProxy.connect(owner).setLockStatus(true,1);
+        await expect(usdtVaultProxy.connect(owner).depositUsdt(6 * 10**6)).
+        to.be.revertedWithCustomError(usdtVaultProxy,"ContractIsNotApproved");
+        await mockUsdt.connect(owner).approve(usdtVaultProxy.target,69 * 10**6);
         await usdtVaultProxy.connect(owner).depositUsdt(69 * 10**6);
         console.log("Contract balance after: "+await mockUsdt.balanceOf(usdtVaultProxy.target));
-        await usdtVaultProxy.connect(orderBookHandler).unlock(user2,7 * 10**6); 
-        await usdtVaultProxy.connect(user2).withdrawUsdt(1 * 10**6);
+        await usdtVaultProxy.connect(orderBookHandler).unlock(user2,70 * 10**6); 
+
+        await expect(usdtVaultProxy.connect(user2).withdrawUsdt(71 * 10**6)).
+        to.be.revertedWithCustomError(usdtVaultProxy,"InsufficientUnlockedBalance");
+        
+        await usdtVaultProxy.connect(user2).withdrawUsdt(69 * 10**6);
+
+        await expect(usdtVaultProxy.connect(user2).withdrawUsdt(1 * 10**6)).
+        to.be.revertedWithCustomError(usdtVaultProxy,"inSufficientBalanceInContract");
         console.log("Contract balance third: "+await mockUsdt.balanceOf(usdtVaultProxy.target));
         console.log("user2 balance after: "+await mockUsdt.balanceOf(user2.address));
     });
