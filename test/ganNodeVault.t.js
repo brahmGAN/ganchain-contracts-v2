542a0048ade8510f29f4fcd06f52843f87cd4405
Deepu <deepuharry02@gmail.com>
2025-07-20 14:45:22 +0530

    GanNodeVault tested

--- test/ganNodeVault.t.js
+++ test/ganNodeVault.t.js
@@ -21,33 +21,29 @@ describe("GANNodeVault", () => {
 
   describe("GanNodeVault::", () => {
     it("Should switch on every user functions", async () => {
-    //   //createSubnet
-    //   await expect(await subnetProxy._createSubnets()).to.be.equals(false);
-    //   await subnetProxy.connect(owner).setUserFunctionStatus(true, 0);
-    //   await expect(await subnetProxy._createSubnets()).to.be.equals(true);
-    //   //deleteSubnet
-    //   await subnetProxy.connect(owner).setUserFunctionStatus(true, 1);
-    //   //claimRewards
-    //   await subnetProxy.connect(owner).setUserFunctionStatus(true, 3);
-    //   //castVotes
-    //   await subnetProxy.connect(owner).setUserFunctionStatus(true, 4);
-    //   //uncastVotes
-    //   await subnetProxy.connect(owner).setUserFunctionStatus(true, 5);
     await ganNode.connect(owner).batchNodeMint([user1],[10],"abcd");
     console.log("Nft balance:"+await ganNode.balanceOf(user1.address));
+    await ganNodeVaultProxy.connect(owner).setLockStatus(true,0);
+    await ganNodeVaultProxy.connect(owner).setLockStatus(true,1);
+    await expect(ganNodeVaultProxy.connect(user1).depositNode(2,[1,2,3])).to
+    .be.revertedWithCustomError(ganNodeVaultProxy,"incorrectArraySize");
     console.log("approval status before:"+await ganNode.isApprovedForAll(user1.address,ganNodeVaultProxy.target));
+    await expect(ganNodeVaultProxy.connect(user1).depositNode(2,[1,2])).to.
+    be.revertedWithCustomError(ganNodeVaultProxy,"contractNotApproved");
     await ganNode.connect(user1).setApprovalForAll(ganNodeVaultProxy.target,true);
     console.log("approval status after:"+await ganNode.isApprovedForAll(user1.address,ganNodeVaultProxy.target));
-    await ganNodeVaultProxy.connect(owner).setLockStatus(true,0);
     await ganNodeVaultProxy.connect(user1).depositNode(2,[1,2]);
     console.log("vault Nft balance:"+await ganNode.balanceOf(ganNodeVaultProxy.target));
-    await ganNodeVaultProxy.connect(orderBookHandler).unlock(user2,2);
+    await expect(ganNodeVaultProxy.connect(owner).unlock(user2,3)).to.
+    be.reverted;
+    await ganNodeVaultProxy.connect(orderBookHandler).unlock(user2,3);
     console.log("user2 Nft balance before:"+await ganNode.balanceOf(user2.address));
-    await ganNodeVaultProxy.connect(owner).setLockStatus(true,1);
-    await ganNodeVaultProxy.connect(user2).withdrawNode(1);
-    console.log("user2 Nft balance after 1st:"+await ganNode.balanceOf(user2.address));
-    await ganNodeVaultProxy.connect(user2).withdrawNode(1);
-    console.log("user2 Nft balance after 2nd:"+await ganNode.balanceOf(user2.address));
+    await expect(ganNodeVaultProxy.connect(user2).withdrawNode(4)).to.
+    be.revertedWithCustomError(ganNodeVaultProxy,"InsufficientUnlockedBalance");
+    await ganNodeVaultProxy.connect(user2).withdrawNode(2);
+    await expect(ganNodeVaultProxy.connect(user2).withdrawNode(1)).to.
+    be.revertedWithCustomError(ganNodeVaultProxy,"inSufficientBalanceInContract");
+    console.log("user2 Nft balance:"+await ganNode.balanceOf(user2.address));
     });
   });
 
